1a102b5397127ffe89f5344ae7e6f89010567a3f
bibiana <bcristofol@gmail.com>
2015-07-31 21:25:36 +0100

    When the Alien is hit, the game is over

--- tests/test_game.py
+++ tests/test_game.py
@@ -19,3 +19,13 @@ def test_switch_turns():
     assert game.turn != player
     game.switch_turns()
     assert game.turn == player
+
+def test_hit():
+    player = Player()
+    alien = Alien(5)
+    game = Game(player, alien)
+    game.hit(1)
+    assert game.over == False
+    game.switch_turns()
+    game.hit(5)
+    assert game.over == True
