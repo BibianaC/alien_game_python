8b9469a644313601a74e4567f8785ef17f751b74
bibiana <bcristofol@gmail.com>
2015-07-31 12:32:32 +0100

    Player can fire

--- alien_game_python/player.py
+++ alien_game_python/player.py
@@ -1,3 +1,12 @@
+import math
+
 class Player(object):
+
+    G = 9.81 # Gravitational acceleration
+    V = 8 # Nozzle velocity v of the spaceship
+
     def __init__(self):
         self.position = 0
+
+    def fire(self, angle, v=V, g=G ):
+        self.impact_area = math.floor((2*math.cos(angle)*math.sin(angle)*v**2)/g)
--- tests/test_player.py
+++ tests/test_player.py
@@ -3,3 +3,8 @@ from alien_game_python.player import Player
 def test_init():
     player = Player()
     assert player.position == 0
+
+def test_fire():
+    player = Player()
+    player.fire(90)
+    assert player.impact_area == -6
