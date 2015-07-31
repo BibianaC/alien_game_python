ae4b89c7ac9665b0e6214568437515cd5cadc289
bibiana <bcristofol@gmail.com>
2015-07-31 11:52:10 +0100

    Alien added move functionality

--- alien_game_python/alien.py
+++ alien_game_python/alien.py
@@ -1,3 +1,15 @@
+import random
+import pdb
+
 class Alien(object):
+
+    BOUNDS = range(-10, 10)
+    MOVES = [-1, 1]
+
     def __init__(self, x_0):
         self.position = x_0
+
+    def move(self, bounds=BOUNDS, moves=MOVES):        
+        move_postion = self.position + random.choice(moves)
+        if move_postion in bounds:
+            self.position = move_postion
--- tests/test_alien.py
+++ tests/test_alien.py
@@ -3,3 +3,14 @@ from alien_game_python.alien import Alien
 def test_init():
     alien = Alien(5)
     assert alien.position == 5
+
+def test_move():
+    alien1 = Alien(5)
+    alien2 = Alien(10)
+    alien3 = Alien(-10)
+    alien1.move()
+    alien2.move()
+    alien3.move()
+    assert alien1.position == 4 or alien1.position == 6
+    assert alien2.position == 9
+    assert alien3.position == -9
